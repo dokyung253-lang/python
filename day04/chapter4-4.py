6812cd7e348596f72b62e78c29a4721fbd31cf6c
dokyung253-lang <dokyung253@gmail.com>
2026-04-20 11:40:51 +0900

    0420 2교시

---
+++ day04/chapter4-4.py
@@ -0,0 +1,56 @@
+# [1] min, max, sum
+numbers = [103, 52, 273, 32, 77]
+print( min(numbers)) #최솟값
+print( max(numbers)) #최댓값
+print( sum(numbers)) #누적합계값
+
+# [2] reversed( 리스트 ), 이터레이터 반환
+print( reversed(numbers)) # <list_reverseiterator object at 0x00000168CC0A13C0>
+
+for i in reversed( numbers ):
+    print(i)        # 역순으로 출력
+
+# [3] enumerate(리스트), 인덱스와 자료 순회 가능하다.(인덱스와 요소값 따로 출력 가능)
+print( enumerate(numbers)) # <enumerate object at 0x0000017B0B5AE980>
+print( list( enumerate(numbers))) #[(0, 103), (1, 52), (2, 273), (3, 32), (4, 77)]
+for index, value in enumerate( numbers ):
+    print(index, value)
+
+# [4] items( )
+example_dictionary = {'keyA' : 'valueA', 'keyB' : "valueB", 'keyC': 'valueC'}
+print(example_dictionary.items()) # dict_items([('keyA', 'valueA'), ('keyB', 'valueB'), ('keyC', 'valueC')])
+
+for key, value in example_dictionary.items() :
+    print( key, value ) 
+
+# [5] 리스트 내 반복문 사용 , 반복문 이용한 리스트 생성
+# ( 0, 20 )사이 짝수 갖는 리스트 
+# (1)
+array = []
+
+for i in range( 0, 20, 2 ) :
+    array.append( i + i )       # append : 추가
+print( array )                  # [0, 4, 8, 12, 16, 20, 24, 28, 32, 36]
+
+# (2) [ 계산식 for 반복변수 in 반복할 수 있는 것 if 조건식 ]
+array  = [i for i in range( 0, 20 , 2) ]
+print( array )
+
+array = [i for i in range(0, 20 , 2) if i in [10, 12] ]
+print( array )
+
+# [6] 문자열 여러줄 입력하기
+# (1) """ """
+print("""안녕하세요1
+안녕하세요2""")
+
+# (2) \n
+print("안녕하세요1\n안녕하세요2")
+
+# (3) () 소괄호 안에서 여러개 문자 연결
+print( ("안녕하세요1\n" "안녕하세요2") )
+
+# (4) 조합문자열.join( 문자열리스트 ), 리스트내 요소 사이에 조합문자열 연결
+print(  "\n".join(["안녕하세요1", "안녕하세요 2"]))
+
+# 
